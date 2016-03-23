bace674abf3268ae8c13259da2bd5eae80883f55
ranzhaocgu <ranzqt@gmail.com>
2016-03-23 14:37:23 -0700

    module 2 materials update

--- Module 2 Quantitative Risk and Return/Module 2 Exam/parta.m
+++ Module 2 Quantitative Risk and Return/Module 2 Exam/parta.m
@@ -6,3 +6,14 @@ r = 0.03;
 Var = R.*(sigma*sigma');
 w = (0.1-r)*inv(Var)*(mu-r) / ((mu-r)'*inv(Var)*(mu-r));
 sigma_p = sqrt(w'*Var*w);
+
+A = ones(1,4)*inv(Var)*ones(4,1);
+B = ones(1,4)*inv(Var)*mu;
+C = mu'*inv(Var)*mu;
+m = (C - B*r) / (B - A*r);
+w_T = (inv(Var)*(mu-r)) / (B - A*r);
+sigma_T = sqrt((C-2*r*B+r^2*A)/(B-A*r)^2);
+slope = (m-r) / sigma_T;
+
+VaR_norm = w_T'*mu + norminv(0.01,0,1)*sigma_T;
+VaR_t = w_T'*mu + tinv(0.01,30)*sigma_T;

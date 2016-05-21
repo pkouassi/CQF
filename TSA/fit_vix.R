eb2da25a753bc1d473f25fe6e4aeeabe81a7ec1b
ranzhaocgu <ranzqt@gmail.com>
2016-05-20 17:14:28 -0700

    Check Aon VA ESG Model

--- TSA/fit_vix.R
+++ TSA/fit_vix.R
@@ -49,8 +49,59 @@ fit.arima.final = arima(d.log.vix.m, order=c(max(long.term),0,0), fixed=fit.para
 
 ############################################################################
 #####################   State Variable Method   ############################
+library(dse)
 
+# univariate model
+vit.fit.dse = dse::SS(F = matrix(-0.06896509, 1, 1), 
+                      Q = matrix(0.1783604, 1, 1),
+                      H = matrix(1, 1, 1), 
+                      R = matrix(0, 1, 1),               
+                      G = matrix(0.4445145, 1, 1),       # k_y * mu_y * dt
+                      constants = list(R = matrix(TRUE, 1, 1)),
+                      z0 = matrix(0, 1, 1), P0 = matrix(10^5, 1, 1))
+vit.m.fit.dse <- estMaxLik(vit.fit.dse, TSdata(input = rep(1,length(log.vix.m)), output = log.vix.m))
+vit.m.fit.dse$model
+k = (1-vit.m.fit.dse$model$F)*12
+mu = (vit.m.fit.dse$model$G/k)*12
+sigma_y = vit.m.fit.dse$model$Q * sqrt(12)
+k
+mu
+sigma_y
 
-
+# two factor model
+f <- array(c(.2,0,.8,.6),c(2,2))
+h <- array(c(1,0,0,1),c(2,2))
+q <- array(c(.1,0,0,.05),c(2,2))
+r <- array(c(1,0,0,1),c(2,2))
+vit.fit.dse.2factor = dse::SS(F = f, Q = q,
+                      H = h, R = r)
+vit.m.fit.dse.2factor <- estMaxLik(vit.fit.dse.2factor, TSdata(output = cbind(log.vix.m,log.vix.m)))
+vit.m.fit.dse.2factor$model
 
 ############################################################################
+
+
+
+
+
+
+data("Nile", package = "datasets")
+m1.dse <- dse::SS(F = matrix(1, 1, 1), Q = matrix(40, 1, 1),
+                  H = matrix(1, 1, 1), R = matrix(130, 1, 1),
+                  z0 = matrix(0, 1, 1), P0 = matrix(10^5, 1, 1))
+m1b.dse <- dse::SS(F = matrix(1, 1, 1), Q = matrix(40, 1, 1),
+                   H = matrix(1, 1, 1), R = matrix(130, 1, 1),
+                   constants = list(Q = matrix(TRUE, 1, 1), P0 = matrix(TRUE, 1, 1)),
+                   z0 = matrix(0, 1, 1), P0 = matrix(10^5, 1, 1))
+m2.dse <- dse::SS(F = matrix(0.5, 2, 2), Q = matrix(0, 1, 1),
+                  H = matrix(1, 2, 2), R = matrix(130, 1, 1),
+                  z0 = matrix(0, 1, 1), P0 = matrix(1, 1, 1))
+m2.dse.est <- estMaxLik(m2.dse, TSdata(output = Nile))
+m2.dse.est <- estMaxLik(m2.dse, TSdata(output = log.vix.m))
+
+f <- array(c(.5,.3,.2,.4),c(2,2))
+h <- array(c(1,0,0,1),c(2,2))
+k <- array(c(.5,.3,.2,.4),c(2,2))
+ss <- SS(F=f,G=NULL,H=h,K=k)
+is.SS(ss)
+ss
